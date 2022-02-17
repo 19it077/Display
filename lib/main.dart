d7a3bb60bb52b5555e532a8e5da326c2a9f36a09
Khushi <19IT077@charusat.edu.in>
2022-02-17 20:39:16 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        body: Center(
+          child: Text(
+            '19IT077 KHUSHI PARIKH.',
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    ),
+  );
+}
